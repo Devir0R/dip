bef7a12fd4cf8eefdc164d914c94259f55f144e5
Dvir <315330670@idf.il>
2019-11-26 17:07:08 +0200

    added link

--- standardplot.py
+++ standardplot.py
@@ -5,10 +5,13 @@ from cv2 import cv2
 
 ###IMAGE READ###
 img = cv2.imread("saber.jpg",cv2.IMREAD_UNCHANGED)
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+img2 = cv2.imread("saber.jpg",cv2.IMREAD_UNCHANGED)
+img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
 ###IMAGE CIRCLE POSITION###
-y,x,a = img.shape
+y,x = img.shape
 x=int(x/2)
 y=int(y/2)
 point = (x,y)
@@ -17,7 +20,7 @@ radius = 5
 ###IMAGE PLOT###
 fig,ax = plt.subplots(1,2)
 ax[0].imshow(img)
-ax[1].imshow(img)
+ax[1].imshow(img2)
 circle = plt.Circle(point, radius=radius, color='r',fill=False)
 ax[0].add_artist(circle)
 ax[0].set_title('Click to move the circle')
@@ -34,8 +37,15 @@ def move_circle(event):
         circle.radius = int(np.sqrt((event.xdata-px)**2+(event.ydata-py)**2))
     elif event.button==1:
         circle.center = event.xdata, event.ydata
+    #change_circle_in_img_2(event)
     fig.canvas.draw()
 
-cid = fig.canvas.mpl_connect('button_press_event', move_circle)
+def change_circle_in_img_2(event):
+    global img2,circle
+    
+
+##https://stackoverflow.com/questions/52365190/blur-a-specific-part-of-an-image
+
+fig.canvas.mpl_connect('button_press_event', move_circle)
 plt.show()
 
